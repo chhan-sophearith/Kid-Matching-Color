85530e368491d2298bdf778ef858515d21ffc864
Chhan Sophearith <44798886+chhan-sophearith@users.noreply.github.com>
2023-07-12 14:39:58 +0700

    init

---
+++ BabySortingToyGame/BabySortingToyGame/BabySortingToyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabySortingToyGameApp.swift
+//  BabySortingToyGame
+//
+//  Created by chhan sophearith on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct BabySortingToyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BabyToyView()
+        }
+    }
+}
